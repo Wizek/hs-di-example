14724520788feca369e115f181d9eb574b412caf
Milán Nagy <123.wizek@gmail.com>
2017-10-28 06:02:21 +0200

    Added a simple virtual file system.

--- test-suite/HsDiExample/MainSpec.hs
+++ test-suite/HsDiExample/MainSpec.hs
@@ -1,10 +1,13 @@
 module HsDiExample.MainSpec where
 
 import            DI                        (assemble, override)
+import qualified  Data.Map                  as M
 import            Data.Time                 (UTCTime, utctDay, addUTCTime)
 import            Test.Hspec
+import            ComposeLTR                ((.>), ($>))
 import            Data.IORef                (newIORef, modifyIORef, readIORef, IORef)
 import            Data.Function             ((&))
+import            Data.Semigroup            ((<>))
 import            HsDiExample.Main          (mainD, mainT)
 import            Data.Functor.Identity     (runIdentity)
 import            Data.Time.Clock.POSIX     (posixSecondsToUTCTime)
@@ -17,10 +20,18 @@ spec = describe "main" $ do
     logs <- newIORef []
     clockStart <- newIORef $ posixSecondsToUTCTime 0
 
+    files <- newIORef mempty
+    let
+      writeFile fname fcont = modifyIORef files (<> (fname =: fcont))
+      readFile fname = readIORef files <&> (
+        (M.lookup fname) .> maybe (error $ "File not found: " <> fname) id)
+
+    writeFile "sample.txt" "Alyssa"
+
     $(mainD
       & override "logger"         [qc| \a -> modifyIORef logs (++ [a]) |]
       & override "getArgs"        [qc| return ["sample.txt"] |]
-      & override "readFile"       [qc| \"sample.txt" -> return "Alyssa" |]
+      & override "readFile"       [qc| readFile |]
       & override "getCurrentTime" [qc| readModifyIORef clockStart (addUTCTime 1) |]
       & assemble)
 
@@ -36,6 +47,8 @@ spec = describe "main" $ do
   it "prints the elapsed time in milliseconds as the second message" $
     (logMessages !! 1) `shouldBe` "1000 milliseconds"
 
+(=:) = M.singleton
+(<&>) = flip (<$>)
 
 readModifyIORef :: IORef a -> (a -> a) -> IO a
 readModifyIORef ref phi = do
